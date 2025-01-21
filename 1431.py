38542c52bbd91cff134b39ff338e3216b7266ba3
tricky toy <trickytoy01@gmail.com>
2025-01-21 22:36:47 +1100

    1431

--- 1431.py
+++ 1431.py
@@ -1,2 +1,15 @@
 def kidsWithCandies(candies, extraCandies):
-    
+    curr_max = max(candies)
+    bool_array = []
+
+    for i in candies:
+        curr_total = i+extraCandies
+        print(curr_total)
+        if (i+extraCandies > curr_max):
+            bool_array.append(True)
+        else:
+            bool_array.append(False)
+
+    return bool_array
+
+print(kidsWithCandies([2,3,5,1,3], 3))
